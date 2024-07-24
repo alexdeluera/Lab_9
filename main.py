15376af0c7ca5e1f9d970b5a6dd037fca4675f51
Alex De Luera <alexdeluera@gmail.com>
2024-07-24 10:33:06 -0400

    Add main function and condition for main to run

--- main.py
+++ main.py
@@ -5,4 +5,21 @@ def encode(password):
         new_str+=str(new_num)
     return new_str
 
-print(encode("12345555"))
+def main():
+    option=0
+    
+    while option!=3:
+        print("\nMenu\n-------------")
+        print("1. Encode\n2. Decode\n3. Quit")
+        option=int(input("Please enter an option: "))
+        if option==1:
+            password=input("Please enter your password to encode: ")
+            encode_pass=encode(password)
+            print("Your password has been encoded and stored!")
+        elif option==2:
+            print(f"The encoded password is {encode_pass}, and the original password is {decode(encode_pass)}.")
+        else:
+            break
+
+if __name__=="__main__":
+    main()
